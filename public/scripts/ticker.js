14c1cf37cef7d5029b6875bb5cad65dadc30de79
GageM <dgmille2@ncsu.edu>
2019-06-26 12:24:25 -0400

    Reddit api call

--- public/scripts/ticker.js
+++ public/scripts/ticker.js
@@ -1,40 +1,49 @@
+const subreddit = "livestreamfail";
+const numberOfPosts = 10;
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+
+const fetchPosts = async () => {
+    const posts = await fetch(`https://www.reddit.com/r/${subreddit}.json`);
+    const responseJSON = await posts.json();
+    formatPosts(responseJSON);
+}
+
+const formatPosts = (responseJSON) => {
+    const posts = [];
+    const labels = [];
+    const data = [];
+    posts.join(...responseJSON.data.children);
+    posts.forEach(({data: {title, score}}) => {
+        labels.push(title);
+        data.push(score);
+    });
+    createChart(labels, data);
+}
+
+const createChart = (labs, dat) => {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labs,
+            datasets: [{
+                label: 'Score',
+                data: dat,
+                borderWidth: 1
             }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
-    }
-});
+    });
+};
+
+fetchPosts();
