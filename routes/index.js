3d3ada0ec044a4024d7e448c31bb0578cae2d2f9
GageM <dgmille2@ncsu.edu>
2019-06-25 22:40:17 -0400

    changed module import

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Charts = require('chart');
+var Charts = require('chart.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
