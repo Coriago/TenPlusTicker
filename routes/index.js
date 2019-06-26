e9c7b63cc7997f0a8e32336499e4c922babbe674
GageM <dgmille2@ncsu.edu>
2019-06-25 23:22:49 -0400

    reconfigure path to support chartjs

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var ChartClass = require('chart.js');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', Chart: ChartClass, P: x});
+  res.render('index', { title: 'Express'});
 });
 
 module.exports = router;
