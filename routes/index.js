b46845d4d7f5f9b76824100238fc394cd0c55c34
GageM <dgmille2@ncsu.edu>
2019-06-25 22:51:09 -0400

    Import Chartjs to index

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var Charts = require('chart.js');
+var x = 5;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', Chart: Charts });
+  res.render('index', { title: 'Express', Chart: Charts, P: x});
 });
 
 module.exports = router;
