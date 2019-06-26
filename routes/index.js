54b5ef546be21e45d49c63864e6fe1a21c7a1411
GageM <dgmille2@ncsu.edu>
2019-06-25 23:09:06 -0400

    include chartjs

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var Charts = require('chart.js');
-var x = 5;
+var ChartClass = require('chart.js');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', P: x});
+  res.render('index', { title: 'Express', Chart: ChartClass, P: x});
 });
 
 module.exports = router;
