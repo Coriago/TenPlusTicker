31a2cfa2cd98d4fae73be3f5e6082dff802b3182
GageM <dgmille2@ncsu.edu>
2019-06-25 22:34:39 -0400

    Add charts linkage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var Charts = require('chart');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', Chart: Charts });
 });
 
 module.exports = router;
